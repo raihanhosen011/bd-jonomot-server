37931a29f57e2b8024d9085a3780bc38c3629f0a
raihanhosen011 <raihanhosen011@gmail.com>
2022-07-10 15:08:49 +0600

    Fix visitors

--- controller/news/getNewsbyId.js
+++ controller/news/getNewsbyId.js
@@ -7,11 +7,11 @@ async function getNewsById(req, res) {
   try {
     const news = await News.findOne({ _id : req.params.id }).populate('publisher', '-password')
     
-    const checkVisitor = await News.findOne({ visitors : ip })
+    // const checkVisitor = await News.findOne({ visitors : ip })
     
-    if(!checkVisitor){
-      await News({ visitors : ip }).save()
-    }
+    // if(!checkVisitor){
+    //   await News({ visitors : ip }).save()
+    // }
 
     res.status(200).json({
       news : news
